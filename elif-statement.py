742f5609a5f91bc8c4dfefa37a5bd7f7440a1433
Chrisstella <99065775@mydavinci.nl>
2021-09-14 11:17:03 +0200

    elif statement

--- elif-statement.py
+++ elif-statement.py
@@ -6,4 +6,6 @@ b = input('geef nog een getal: ')
 if a > b:
     Max = a
     print("a is het grootste getal: " + str(Max))
-
+elif a < b:
+    Min = a
+    print("a is het kleinste getal: " + str(Min))
